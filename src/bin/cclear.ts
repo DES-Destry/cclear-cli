b72b07f1822c790062120f8b419323a853619411
DES-Destry <agafonovandrej69@gmail.com>
2022-03-22 16:33:16 +0600

    fix: add cleaners selecting

--- src/bin/cclear.ts
+++ src/bin/cclear.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
+import inquirer from 'inquirer';
+import { CliConfig } from '../lib/config/cli.config.js';
 import { CheckPlatform } from '../lib/util/check-platform.js';
 
-function main() {
-    console.log('CLI working');
+async function main() {
+    const selectedCleaners: { cleaners: string[] } = await inquirer.prompt({
+        name: 'cleaners',
+        type: 'checkbox',
+        message: 'What kind of cache do you want to clean?',
+        choices: CliConfig.AvailableCleaners,
+    });
 }
 
 new CheckPlatform().start();
-main();
-
+await main();
--- src/lib/config/cli.config.ts
+++ src/lib/config/cli.config.ts
@@ -2,4 +2,6 @@ export class CliConfig {
     // TODO: make win32 and linux versions
     public static readonly SupportedPlatforms = ['darwin'];
     public static readonly CurrentPlatform = process.platform;
+
+    public static readonly AvailableCleaners = ['System', 'Docker', 'Nuget', 'Npm', 'VSCode'];
 }
