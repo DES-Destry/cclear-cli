bf6cf1731ecc2db7471ded98e2296457c258ad60
DES-Destry <agafonovandrej69@gmail.com>
2022-03-22 13:51:24 +0600

    feat: add OS checking

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log('CLI working');
+import chalk from 'chalk'
+
+// TODO: make win32 and linux versions
+const supportedOS = ['darwin'];
+const currentOS = process.platform;
+
+if (!supportedOS.includes(currentOS)) {
+    unsupportedOS();
+} else {
+    await executeCli();
+}
+
+
+async function executeCli() {
+    console.log('CLI working');
+}
+
+function unsupportedOS() {
+    console.log(chalk.bgRed('Your operating system are not supported =('));
+    console.log(`Supported OS: ${chalk.green(supportedOS)}`);
+    console.log(`Current OS: ${currentOS}`);
+}
